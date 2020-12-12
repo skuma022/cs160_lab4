06e8c49b537b13de42f98a1fbdd02820cefe0a80
skuma022 <41928678+skuma022@users.noreply.github.com>
2020-12-11 22:43:44 -0800

    Create num1.cpp

---
+++ num1.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include "omp.h"
+
+static long num_steps = 10000000; 
+double step;
+#define NUM_THREADS 4
+
+
+void main()
+{
+	for (int j = NUM_THREADS-1; j >= 0; j--) {
+	int i, nthreads; double pi, sum[NUM_THREADS];
+	step = 1.0 / (double)num_steps;
+	double runtime = omp_get_wtime();
+	omp_set_num_threads(NUM_THREADS - j);
+	#pragma omp parallel
+	{
+		int i, id, nthreads;
+		double x;
+		id = omp_get_thread_num();
+		nthrds = omp_get_num_threads();
+		if (id == 0) nthreads = nthreads;
+		for (i = id, sum[id] = 0.0; i < num_steps; i = i + nthreads) {
+			x = (i + 0.5) * step;
+	#pragma omp critical
+			sum[id] = sum[id] + 4.0 / (1.0 + x * x);
+		}
+	}
+	for (i = 0, pi = 0.0; i < nthreads; i++) {
+		pi = pi + sum[i] * step;
+
+	}
+	runtime = omp_get_wtime() - runtime;
+	printf("pi = % f in % f secs with % d threads\n", pi, runtime, (NUM_THREADS - j));
+	}
+  return;
+}
